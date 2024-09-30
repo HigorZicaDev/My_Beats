d43438657d8f208d08160a98735ed66d0872d8c7
HigorZicaDev <hztech.dev.fr@gmail.com>
2024-09-30 14:38:45 +0200

    Feature metadata image:og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Montserrat } from 'next/font/google';
+import logo from "@/assets/logo.svg";
 
 
 export const metadata: Metadata = {
   title: "My-Beats",
   description: "App pour partager des Beats and Musics Free.",
+  openGraph: {
+    images: logo,
+  },
 };
 
 const font = Montserrat({
